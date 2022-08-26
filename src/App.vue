729751dade11f99e0085087cfb0d0461bdfca4f6
Vadim Igumnov <vad.igumnoff@gmail.com>
2022-08-26 11:57:51 +0300

    init: study-tracker project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ hello }}
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const hello = ref('single');
+
+    return { hello };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
